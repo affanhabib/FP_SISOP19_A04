ff69ac6fc42ff913cd8bd74432fca2e42973940a
Affan Ahsanul Habib <ahsanulhabib11@gmail.com>
2019-05-12 22:21:37 +0700

    coba

---
+++ fuse.c
@@ -0,0 +1,35 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+
+static int xmp_getattr()
+{
+
+}
+
+static int xmp_readdir()
+{
+
+}
+
+static int xmp_read()
+{
+
+}
+
+static struct fuse_operations xmp_oper = {
+	.getattr	= xmp_getattr,
+	.readdir	= xmp_readdir,
+	.read		= xmp_read,
+};
+
+int main(int argc, char *argv[])
+{
+	
+}
